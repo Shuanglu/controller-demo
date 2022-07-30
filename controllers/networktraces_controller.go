a1e39e187a0876d8f69d4eae66a1614bd3c05801
Shuang Lu <shuanglu@tubi.tv>
2022-07-30 21:28:22 +0800

    update

--- controllers/networktraces_controller.go
+++ controllers/networktraces_controller.go
@@ -1,17 +1,17 @@
 /*
-Copyright 2022.
+	Copyright 2022.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+		http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 */
 
 package controllers
@@ -25,6 +25,12 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	k8sdebuggerv1 "shuanglu/k8s-debugger/api/v1"
+
+	kbatch "k8s.io/api/batch/v1"
+)
+
+var (
+	jobOwnerKey = ".metadata.controller"
 )
 
 // NetworktracesReconciler reconciles a Networktraces object
@@ -51,6 +57,18 @@ func (r *NetworktracesReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 
 	// TODO(user): your logic here
 
+	// Fetch the networktraces objects
+	var networktraces k8sdebuggerv1.Networktraces
+	if err := r.Get(ctx, req.NamespacedName, &networktraces); err != nil {
+		log.Log.Error(err, "unable to fetch Networktraces")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	var childJobs kbatch.JobList
+	if err := r.List(ctx, &childJobs, client.InNamespace(req.Namespace), client.MatchingFields{jobOwnerKey: req.Name}); err != nil {
+		log.Log.Error(err, "unable to list Jobs")
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
